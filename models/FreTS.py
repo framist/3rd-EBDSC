5563f66b8e10f24d8e04a547fa9524347a4d2bef
framist <framist@163.com>
2025-02-27 22:27:05 +0800

    FreTS for iq

--- models/FreTS.py
+++ models/FreTS.py
@@ -15,12 +15,12 @@ class FreTS_Block(nn.Module):
         self.hidden_size = hidden_size
         self.sparsity_threshold = sparsity_threshold
         self.scale = scale
-        self.r1 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.embed_size))
-        self.i1 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.embed_size))
-        self.rb1 = nn.Parameter(self.scale * torch.randn(self.embed_size))
-        self.ib1 = nn.Parameter(self.scale * torch.randn(self.embed_size))
-        self.r2 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.embed_size))
-        self.i2 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.embed_size))
+        self.r1 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.hidden_size))
+        self.i1 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.hidden_size))
+        self.rb1 = nn.Parameter(self.scale * torch.randn(self.hidden_size))
+        self.ib1 = nn.Parameter(self.scale * torch.randn(self.hidden_size))
+        self.r2 = nn.Parameter(self.scale * torch.randn(self.hidden_size, self.embed_size))
+        self.i2 = nn.Parameter(self.scale * torch.randn(self.hidden_size, self.embed_size))
         self.rb2 = nn.Parameter(self.scale * torch.randn(self.embed_size))
         self.ib2 = nn.Parameter(self.scale * torch.randn(self.embed_size))
 
@@ -37,21 +37,11 @@ class FreTS_Block(nn.Module):
     def MLP_temporal_complex(self, x, B, L):
         # [B, T, D]
         x = torch.fft.fft(x, dim=1, norm='ortho')  # FFT on L dimension
-        y = self.FreMLP_complex(B, L, x, self.r2, self.i2, self.rb2, self.ib2)
+        y = self.FreMLP_complex1(B, L, x, self.r1, self.i1, self.rb1, self.ib1)
+        y = self.FreMLP_complex2(B, L, y, self.r2, self.i2, self.rb2, self.ib2)
         x = torch.fft.ifft(y, n=L, dim=1, norm="ortho")
         return x
 
-    # frequency channel learner
-    def MLP_channel(self, x, B, N, L):
-        # [B, N, T, D]
-        x = x.permute(0, 2, 1, 3)
-        # [B, T, N, D]
-        x = torch.fft.rfft(x, dim=2, norm='ortho')  # FFT on N dimension
-        y = self.FreMLP(B, L, N, x, self.r1, self.i1, self.rb1, self.ib1)
-        x = torch.fft.irfft(y, n=N, dim=2, norm="ortho")
-        x = x.permute(0, 2, 1, 3)
-        # [B, N, T, D]``
-        return x
 
     # frequency-domain MLPs
     # dimension: FFT along the dimension, r: the real part of weights, i: the imaginary part of weights
@@ -79,26 +69,48 @@ class FreTS_Block(nn.Module):
         y = torch.view_as_complex(y)
         return y
 
-    def FreMLP_complex(self, B, dimension, x, r, i, rb, ib):
+    def FreMLP_complex1(self, B, dimension, x, r, i, rb, ib):
         o1_real = torch.zeros([B, dimension, self.embed_size],
                               device=x.device)
         o1_imag = torch.zeros([B, dimension, self.embed_size],
                               device=x.device)
 
         o1_real = F.relu(
-            torch.einsum('bid,dd->bid', x.real, r) - \
-            torch.einsum('bid,dd->bid', x.imag, i) + \
+            torch.einsum('bid,dh->bih', x.real, r) - \
+            torch.einsum('bid,dh->bih', x.imag, i) + \
             rb
         )
 
         o1_imag = F.relu(
-            torch.einsum('bid,dd->bid', x.imag, r) + \
-            torch.einsum('bid,dd->bid', x.real, i) + \
+            torch.einsum('bid,dh->bih', x.imag, r) + \
+            torch.einsum('bid,dh->bih', x.real, i) + \
             ib
         )
 
         y = torch.stack([o1_real, o1_imag], dim=-1)
         y = F.softshrink(y, lambd=self.sparsity_threshold)
+        y = torch.view_as_complex(y)
+        return y
+
+    def FreMLP_complex2(self, B, dimension, x, r, i, rb, ib):
+        o1_real = torch.zeros([B, dimension, self.embed_size],
+                              device=x.device)
+        o1_imag = torch.zeros([B, dimension, self.embed_size],
+                              device=x.device)
+
+        o1_real = F.relu(
+            torch.einsum('bih,hd->bid', x.real, r) - \
+            torch.einsum('bih,hd->bid', x.imag, i) + \
+            rb
+        )
+
+        o1_imag = F.relu(
+            torch.einsum('bih,hd->bid', x.imag, r) + \
+            torch.einsum('bih,hd->bid', x.real, i) + \
+            ib
+        )
+
+        y = torch.stack([o1_real, o1_imag], dim=-1)
         y = torch.view_as_complex(y)
         return y
 
@@ -140,7 +152,10 @@ class FreTS_Block(nn.Module):
         # 转为复数 N=2 as complex number's real and imaginary part
         x = x.permute(0, 2, 3, 1)
         x = torch.complex(x[..., 0], x[..., 1])
-        x = x.to(torch.complex64)
+        
+        # amp 时 T 不是 2 的幂次方时，cuFFT 会报错，需要 x.to(torch.complex64)
+        # cuFFT only supports dimensions whose sizes are powers of two when computing in half precision
+        # x = x.to(torch.complex64)
         
         x = self.MLP_temporal_complex(x, B, T)
         
